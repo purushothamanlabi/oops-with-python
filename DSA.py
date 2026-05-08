ac1adc57f6deff691d7e4467bf106e182671b9a3
purushothamanlabi <purushothaman.abi.2003@gmail.com>
2026-05-08 22:20:13 +0530

    feat: add documentation outlining data structures and algorithms overview

---
+++ DSA.py
@@ -0,0 +1,69 @@
+"""
+DSA - complete learning 
+
+Types:
+    1. Linear Data Structures
+    2. Non-Linear Data Structures
+
+
+1. Linear Data Structures:
+
+    1. Array
+        - Static Array
+        - Dynamic Array
+        - Sorted Array
+        - Multi-dimensional Array
+
+    2. Linked List
+        - Singly Linked List
+        - Doubly Linked List
+        - Circular Linked List
+        - Circular Doubly Linked List
+
+    3. Stack
+        - Simple Stack
+        - Monotonic Stack
+
+    4. Queue
+        - Simple Queue
+        - Circular Queue
+        - Deque
+        - Priority Queue
+
+
+2. Non-Linear Data Structures:
+
+    1. Hash-based Structures
+        - Hash Table
+        - Hash Map
+        - Hash Set
+        - Set
+
+    2. Tree-based Structures
+        - Tree
+        - Binary Tree
+        - Binary Search Tree
+        - AVL Tree
+        - Red-Black Tree
+        - B-Tree
+        - B+ Tree
+        - Heap
+        - Trie
+        - Segment Tree
+        - Fenwick Tree
+        - Disjoint Set / Union-Find
+
+    3. Graph-based Structures
+        - Graph
+        - Directed Graph
+        - Undirected Graph
+        - Weighted Graph
+        - Unweighted Graph
+        - Connected Graph
+        - Disconnected Graph
+        - Cyclic Graph
+        - Acyclic Graph
+
+    4. Optional / Advanced
+        - Skip List
+"""
